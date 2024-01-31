78089c278a3d9135c0d28bc51d9231f0eedb67d4
Noam-Mellul <mellul.noam@post.runi.ac.il>
2024-01-31 19:27:05 +0200

    HW7

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,7 +14,7 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 		In in = new In(fileName);
 
-		for (int i = 0; i < dictionary.length; i++){
+		for (int i = 0; i < dictionary.length; i++){// loop for in order to put the dictionary words in the array
 			dictionary[i] = in.readString();
 		}
 
@@ -24,7 +24,7 @@ public class HashTagTokenizer {
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
 		for(int i = 0; i < dictionary.length; i++){
-			if (dictionary[i].equals(word)){
+			if (dictionary[i].equals(word)){ // check if the given word is in the dictionary
 				return true;
 			}
 		}
@@ -37,16 +37,16 @@ public class HashTagTokenizer {
 		if (hashtag.isEmpty()) {
 			return;
 		}
-		hashtag = hashtag.toLowerCase();
+		hashtag = hashtag.toLowerCase();// the word to lower case
 		int N = hashtag.length();
 
 		for (int i = 1; i <= N; i++) {
-			String hashString = hashtag.substring(0,i);
-			if (existInDictionary(hashString, dictionary) == true) {
-				System.out.println(hashString);
-				String hashStringNext = hashtag.substring(i);
-				breakHashTag(hashStringNext, dictionary);
-				return;
+			String hashString = hashtag.substring(0,i); // create a new string from the index 0 to i (i not include), from the given string
+			if (existInDictionary(hashString, dictionary) == true) { // if the substring is in the dictionary
+				System.out.println(hashString);// print it
+				String hashStringNext = hashtag.substring(i);// create a new string from index i, from the given string for the next iteration
+				breakHashTag(hashStringNext, dictionary); // recursive call
+				return; // stop the recursive call
 			}
 		}
 	}
--- SpellChecker.java
+++ SpellChecker.java
@@ -12,12 +12,12 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		// Your code goes here
-		return str.substring(1);
+		return str.substring(1); // return a substring from index 1, from the given string
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
-		word1 = word1.toLowerCase(); 
+		word1 = word1.toLowerCase();
 		word2 = word2.toLowerCase();
 		if (word1.length() == 0){
 			return word2.length();
@@ -25,10 +25,11 @@ public class SpellChecker {
 		if (word2.length() == 0){
 			return word1.length();
 		}
+		// if the first letter of each word is the same
 		if (word1.charAt(0) == word2.charAt(0)){
-			return levenshtein(tail(word1), tail(word2));
+			return levenshtein(tail(word1), tail(word2));// recursive call without the first letter of each word
 		}
-
+        // return the sum of 1 and the minimum of the remaining possibilities
 		return 1 + Math.min(Math.min(levenshtein(tail(word1), word2) , levenshtein(word1, tail(word2))) , levenshtein(tail(word1), tail(word2)));
 	}
 
@@ -36,7 +37,7 @@ public class SpellChecker {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-		for (int i = 0; i < dictionary.length; i++){
+		for (int i = 0; i < dictionary.length; i++){// loop for in order to put the dictionary words in the array
 			dictionary[i] = in.readString();
 		}
 		return dictionary;
@@ -44,12 +45,14 @@ public class SpellChecker {
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
-		String target = word;
+		String target = word;// the target, so it's the word given or word that closely resembles
+		int min = threshold + 1;
 		for(int i = 0; i < dictionary.length; i++){
-			int lev = levenshtein(dictionary[i], word);
-			if(lev <= threshold){
-				target = dictionary[i];
-				lev = threshold;
+			int lev = levenshtein(dictionary[i], word);// the distance between the given word and the word in the dictionary at index i
+			if(lev < min){
+				min = lev; // change the value of the minimum
+				target = dictionary[i]; // the target is now this word
+
 			}
 		}
 		return target;
